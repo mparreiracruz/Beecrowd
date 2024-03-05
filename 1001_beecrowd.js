6dae123984186de9a616424a465c6f849a03f390
Matheus <matheusparreira.cruz@gmail.com>
2024-03-05 20:46:25 -0300

    Initial commit

---
+++ 1001_beecrowd.js
@@ -0,0 +1,19 @@
+/**
+ * Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma de A e B atribuindo o seu resultado na variável X. Imprima X conforme exemplo apresentado abaixo. Não apresente mensagem alguma além daquilo que está sendo especificado e não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
+
+Entrada
+A entrada contém 2 valores inteiros.
+
+Saída
+, conforme o exemplo abaixo.
+ * 
+ */
+var input = require('fs').readFileSync('stdin', 'utf8')//configuracao
+var lines = input.split('\n');//configuracao
+
+var A = parseInt(lines.shift());//Entrada
+var B = parseInt(lines.shift());//Entrada
+
+var X = A + B;//Processamento
+
+console.log('X = ' + X);//Saída
