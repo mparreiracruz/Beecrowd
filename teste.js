fd0a170489c64433c14689e360828ace95f6d828
Matheus <matheusparreira.cruz@gmail.com>
2024-03-08 01:20:30 -0300

    Novo

--- teste.js
+++
@@ -1,9 +0,0 @@
-var input = require('fs').readFileSync('stdin', 'utf8')//configuracao
-var lines = input.split('\n');//configuracao
-
-var A = parseInt(lines.shift());//Entrada
-var B = parseInt(lines.shift());//Entrada
-
-var X = A + B;//Processamento
-
-console.log('X = ' + X);//Saída
