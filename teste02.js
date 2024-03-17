2739c31190e1a8c33da0208d428c0983830a2b7e
Matheus <matheusparreira.cruz@gmail.com>
2024-03-16 22:18:33 -0300

    Novo

--- teste02.js
+++
@@ -1,8 +0,0 @@
-var input = require('fs').readFileSync('stdin', 'utf8');
-
-var lines = input.split(' ');
-
-var linha1 = lines.shift(); //configuracao de saída
-var linha2 = linhaPrincipal.shift().split(" ");
-
-console.log(lines);
