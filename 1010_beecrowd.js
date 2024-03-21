5a080b6ec3ec6eb689a3e2e2dbf7c6a1ad6fa50c
Matheus <147951308+mparreiracruz@users.noreply.github.com>
2024-03-21 20:22:41 -0300

    Update 1010_beecrowd.js
    Com explicação de gestão de Strings.

--- 1010_beecrowd.js
+++ 1010_beecrowd.js
@@ -21,18 +21,17 @@ Exemplo de entrada:                   Exemplo de saída:
 161 4 5.20
 */
 var input = require("fs").readFileSync("stdin","utf8");
-var lines = input.split("\n");
+var lines = input.split("\n");// lines = ["12 1 5.30" ,  "16 2 5.10"]
 
-var line1 = lines.shift();
+var line1 = lines.shift();//line1 = ["12 1 5.30"]
+var line2 = lines.shift();//line2 = ["16 2 5.10"]
 
-var line2 = lines.shift();
-
-var Produto01 = line1.split(" ");
+var Produto01 = line1.split(" ");//Produto01 = ["12" , "1" , "5.30"]
 var codiogoProduto1 = parseInt(Produto01.shift());
 var numeroDePeca1 = parseInt(Produto01.shift());
 var valorUnitarioDeCadaPeca1     = parseFloat(Produto01.shift());
 
-var Produto02 = line2.split(" ");
+var Produto02 = line2.split(" ");//Produto02 = ["16" , "2" , "5.10"]
 var condigoProduto2 = parseInt(Produto02.shift());
 var numeroDePeca2 = parseInt(Produto02.shift());
 var valorUnitarioDeCadaPeca2 = parseFloat(Produto02.shift());
