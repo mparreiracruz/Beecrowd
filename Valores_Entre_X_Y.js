64e952e31517664b8f1d6e84403560f6a83bbf4c
Matheus <matheusparreira.cruz@gmail.com>
2024-03-27 14:40:33 -0300

    Novo

--- Valores_Entre_X_Y.js
+++ Valores_Entre_X_Y.js
@@ -4,17 +4,11 @@ var lines = input.split('\n')
 var X = parseInt(lines.shift())
 var Y = parseInt(lines.shift())
 
-var contadorEntreXY = 0
+//var somaImpares = 0 
 for (var i = X + 1; i < Y; i++) {
-    contadorEntreXY++
-    console.log(contadorEntreXY)
+    if (Math.abs(i % 2) == 1) {
+    var somaImpares = i + i
+    console.log(somaImpares)
 }
-
-/**
- * 
-for (var n = 1; n <= 100; n++) {
-    if (n % 2 == 0) {
-        console.log(n);
-    }
 }
- */
+//Leia 2 valores inteiros X e Y. A seguir, calcule e mostre a soma dos números //impares entre eles.
